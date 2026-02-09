b31b4388c3c7d467305ed4890534d99b78c78be4
Thamielis <52717133+Thamielis@users.noreply.github.com>
2026-02-09 18:13:22 +0100

    feat(script): add ESXi data collector and sample export

--- bin/Get-ESXiData.ps1
+++ bin/Get-ESXiData.ps1
@@ -1,12 +1,81 @@
+
+Import-Module VMware.VimAutomation.Core -MinimumVersion 13.4
+
 #region Connect to vCenter Server
-$VIServer     = 'myvcenterserver.local'
-$VICredential = Get-Credential
-Connect-VIServer $VIServer -Credential $VICredential
+$VIServer = 'at-kl-vcenter.kostweingroup.intern'
+#$VICredential = Get-Credential
+Connect-VIServer $VIServer #-Credential $VICredential
 #endregion
 
 #region Get ESXiHosts
-$vmHosts = Get-VMHost -Location 'MyDatacenter'
-foreach($node in $vmHosts){
+$DataCenter = 'Kostwein'
+$vmHosts = Get-VMHost -Location $DataCenter | Sort-Object -Property Name
+
+$Locations = @{
+    'AT-KL' = 'Klagenfurt'
+    'AT-VK' = 'Völkermarkt'
+    'HR-TR' = 'Trnovec'
+    'HR-VA' = 'Varazdin'
+    'IN-AB' = 'Ahmedabad'
+    'IT-UD' = 'Udine'
+    'US-GR' = 'Greenville'
+}
+
+$ESXiServer = foreach ($node in $vmHosts) {
+
+    $LocationKey = $Locations.Keys | Where-Object { $vmHosts[0].Name -imatch "^$_" }
+    $Location = $Locations.$LocationKey
+
+    [PSCustomObject]@{
+        Id               = $node.Id
+        HostName         = $node.Name
+        Version          = $node.Version
+        Manufacturer     = $node.Manufacturer
+        Model            = $node.Model
+        vCenterServer    = $VIServer
+        Cluster          = $Parent
+        PhysicalLocation = $Location
+        ConnectionState  = $node.ConnectionState
+        Notes            = ''
+    }
+
+    $VMs = foreach ($VM in $node.ExtensionData.Vm) {
+        [PSCustomObject]@{
+            Id       = $VM.Value
+            HostName = ''
+        }
+    }
+
+    $DataStores = foreach ($Store in $node.ExtensionData.Datastore) {
+        [PSCustomObject]@{
+            Id   = $Store.Value
+            Name = ''
+        }
+    }
+
+    $NetworkInfo = [PSCustomObject]@{
+        Id       = $node.NetworkInfo.Id
+        Name     = $node.NetworkInfo.Name
+        HostName = $node.NetworkInfo.HostName
+        VMHostId = $node.NetworkInfo.VMHostId
+    }
+
+    $StorageInfo = [PSCustomObject]@{
+        Id       = $node.StorageInfo.Id
+        Name     = $node.StorageInfo.Name
+        VMHostId = $node.StorageInfo.VMHostId
+    }
+    
+}
+
+$ESXiServer | ConvertTo-Csv -Delimiter ';' | Out-File ".\..\data\KOW_ESXiHosts.csv" -Encoding UTF8 -Force
+
+
+
+##########################################################################################
+
+
+foreach ($node in $vmHosts) {
     [PSCustomObject]@{
         HostName         = $node.Name
         Version          = $node.Version
